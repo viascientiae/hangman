80f2fbe84662900d62a70b24e7d6f9b1d1480e59
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2023-12-30 17:34:53 +0530

    Implement saving game and loading saved games

--- hangman.rb
+++ hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 # # Check to ensure the dictionary file is available
 # dictionary_available = (File.exist? "google-10000-english-no-swears.txt") ? "Yes" : "No"
 # puts "Is the dictionary available? : #{dictionary_available}"
@@ -10,10 +12,8 @@ class Computer
   end
 
   def select_secret_code
-    # Load the dictionary as an array of words that are 5-12 characters in length and with the newline character at their ends removed
     dictionary = File.readlines("google-10000-english-no-swears.txt").map { |word| word.chomp }.filter { |word| word.length >= 5 && word.length <= 12 }
 
-    # Randomly choose a secret word from the dictionary as loaded above
     dictionary.sample
   end
 end
@@ -33,84 +33,110 @@ class Game
   def initialize(player_name, computer)
     @player_name = player_name.to_s
     @secret_word = computer.secret_word
+    @turn = 1
+    @incorrect_guesses = 0
+    @secret_word_display = "_" * @secret_word.length
   end
 
   def play
-    # # Display the secret word for testing purposes
-    # puts "Secret Word: #{@secret_word}"
 
     secret_word_length = @secret_word.length
-    secret_word_display = []
 
-    i = 0
-    while i < secret_word_length do
-    secret_word_display.push("_")
-    i += 1
-    end
+    @secret_word_display = @secret_word_display
 
-    secret_word_display = secret_word_display.join
-
-    turn = 1
-    incorrect_guesses = 0
     incorrect_guesses_allowed = 6
     secret_word_array = @secret_word.split("")
 
     puts "\n-------------------------------------------------------------------------------"
-    puts "Let's begin the game. You're allowed only #{incorrect_guesses_allowed} incorrect guesses."
+    puts "Let's begin the game. You're allowed only #{incorrect_guesses_allowed - @incorrect_guesses} incorrect guesses."
     puts "-------------------------------------------------------------------------------\n"
-    # Main game logic loop
-    while incorrect_guesses <= incorrect_guesses_allowed do
-      puts "\nTurn ##{turn}"
+    while @incorrect_guesses <= incorrect_guesses_allowed do
+      puts "\nTurn ##{@turn}"
       puts "-------------------------------------------------------------------------------"
-      puts "\nSecret Word: #{secret_word_display}\n\n(Secret Word Length: #{secret_word_length})\n(Number of incorrect guesses remaining: #{incorrect_guesses_allowed - incorrect_guesses})"
-      print "\n#{@player_name}, please guess a letter of the secret word or the whole secret word itself: "
-      player_guess = gets.downcase.chomp
-
-      if player_guess.length == 1
-        if secret_word_array.any?(player_guess)
-          secret_word_array.each_with_index do |letter, index|
-            if letter == player_guess
-              secret_word_display[index] = player_guess
+      puts "\nSecret Word: #{@secret_word_display}\n\n(Secret Word Length: #{secret_word_length})\n(Number of incorrect guesses remaining: #{incorrect_guesses_allowed - @incorrect_guesses})"
+      print "\nTo continue the game enter 1 or to save the game enter 2: "
+      choice = gets.chomp
+      if choice == "1"
+        print "\n#{@player_name}, please guess a letter of the secret word or the whole secret word itself: "
+        player_guess = gets.downcase.chomp
+        if player_guess.length == 1
+          if secret_word_array.any?(player_guess)
+            secret_word_array.each_with_index do |letter, index|
+              if letter == player_guess
+                @secret_word_display[index] = player_guess
+              end
             end
+            if @secret_word_display == secret_word_array
+              puts "\nSecret Word: #{@secret_word_display}\n\n(Secret Word Length: #{secret_word_length})\n(Number of incorrect guesses remaining: #{incorrect_guesses_allowed - @incorrect_guesses})"
+              puts "\nCongratulations #{@player_name}! You've won the game! You've correctly guessed the secret word."
+              puts "-------------------------------------------------------------------------------\n\n"
+              break
+            end
+          else
+            @incorrect_guesses += 1
           end
-          if secret_word_display == secret_word_array.join
-            puts "\nSecret Word: #{secret_word_display}\n\n(Secret Word Length: #{secret_word_length})\n(Number of incorrect guesses remaining: #{incorrect_guesses_allowed - incorrect_guesses})"
+        elsif player_guess.length > 1
+          if player_guess == secret_word_array
+            @secret_word_display = player_guess
+            puts "\nSecret Word: #{@secret_word_display}\n\n(Secret Word Length: #{secret_word_length})\n(Number of incorrect guesses remaining: #{incorrect_guesses_allowed - @incorrect_guesses})"
             puts "\nCongratulations #{@player_name}! You've won the game! You've correctly guessed the secret word."
             puts "-------------------------------------------------------------------------------\n\n"
             break
+          else
+            @incorrect_guesses += 1
           end
-        else
-          incorrect_guesses += 1
         end
-      elsif player_guess.length > 1
-        if player_guess == secret_word_array.join
-          secret_word_display = player_guess
-          puts "\nSecret Word: #{secret_word_display}\n\n(Secret Word Length: #{secret_word_length})\n(Number of incorrect guesses remaining: #{incorrect_guesses_allowed - incorrect_guesses})"
-          puts "\nCongratulations #{@player_name}! You've won the game! You've correctly guessed the secret word."
+
+        if @incorrect_guesses == 6
+          puts "\nYou've lost #{@player_name}.\n\nYou've run out of allowed number of incorrect guesses.\n\nThe secret word was #{secret_word}"
           puts "-------------------------------------------------------------------------------\n\n"
           break
-        else
-          incorrect_guesses += 1
         end
-      end
 
-      if incorrect_guesses == 6
-        puts "\nYou've lost #{@player_name}.\n\nYou've run out of allowed number of incorrect guesses.\n\nThe secret word was #{secret_word}"
         puts "-------------------------------------------------------------------------------\n\n"
+
+        @turn += 1
+
+      elsif choice == "2"
+        print "\nPlease enter a filename for saving the game without the file extension: "
+        filename = gets.chomp + ".yml"
+        File.open(filename, 'w') { |file| file.write(YAML::dump(self))}
+        puts "\nThe game has been saved."
         break
       end
-
-      puts "-------------------------------------------------------------------------------\n\n"
-
-      turn += 1
-    end
     end
   end
+end
 
-puts "Welcome to Hangman!"
-print "\nPlease enter your name to start the game: "
-player_name = gets.chomp
-computer = Computer.new
-player = Player.new(player_name)
-game = Game.new(player_name, computer)
+def load_game
+  print "\nWelcome to Hangman!\n\nWould you like to play a new game or load a saved game?\n\nEnter yes for a new game and no for loading a saved game (yes/no) "
+  choice = gets.chomp
+  if choice == "no"
+    print "\nEnter the filename of the saved game you want to load without the file extension: "
+    filename = gets.chomp + ".yml"
+    if File.exist?(filename) && !File.zero?(filename)
+      print "\nA saved game was found by that filename. Do you want to resume? (yes/no): "
+      answer = gets.chomp.downcase
+      if answer == "yes"
+        return YAML.safe_load(File.read(filename), permitted_classes: [Game, Player, Computer])
+      end
+    end
+  else
+    create_new_game
+  end
+end
+
+def create_new_game
+  player_name = prompt_player_name
+  computer = Computer.new
+  player = Player.new(player_name)
+  Game.new(player_name, computer)
+end
+
+def prompt_player_name
+  print "\nPlease enter your name to start a new game: "
+  gets.chomp
+end
+
+game = load_game
 game.play
